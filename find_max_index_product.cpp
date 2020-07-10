0ea9fa33a98f259b2ce7826b9686982517d4d545
subhrapaladhi <subhrapaladhi9@gmail.com>
2020-07-10 16:06:05 +0530

    find max index pro completed

--- find_max_index_product.cpp
+++ find_max_index_product.cpp
@@ -2,43 +2,64 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> nextGreatestLeft(vector<int> a){
-    stack<int> stk;
-    vector<int> left_index(a.size(),0);
-    for(int i =a.size()-1; i>=0; i--){
-        while(stk.empty() && a[i]>a[stk.top()]){
-            int r = stk.top();
+vector<long long> nextGreatestLeft(vector<long long> a){
+    long long n = a.size();
+    stack<long long> stk;
+    vector<long long> left_index(n);
+    
+    left_index[0] = 0;
+    stk.push(0);
+
+    for(int i=1; i<n; i++){
+        while(!stk.empty() && a[i]>=a[stk.top()]){
             stk.pop();
-            left_index[r] = i;
+        }
+        
+        if(stk.empty()){
+            left_index[i] = 0;
+        } else {
+            left_index[i] = stk.top()+1;
         }
         stk.push(i);
     }
+
     return left_index;
 }
 
-vector<int> nextGreatestRight(vector<int> a){
-    stack<int> stk;
-    vector<int> right_index(a.size(),0);
+vector<long long> nextGreatestRight(vector<long long> a){
+    long long n = a.size();
+    stack<long long> stk;
+    vector<long long> right_index(n);
 
-    for(int i=0; i<a.size(); i++){
-        while(stk.empty() && a[i]>a[stk.top()]){
-            int l = stk.top();
+    right_index[n-1] = 0;
+    stk.push(n-1);
+
+    for(int i=n-2; i>=0; i--){
+        while(!stk.empty() && a[i]>=a[stk.top()]){
             stk.pop();
-            right_index[l] = i;
+        }
+
+        if(stk.empty()){
+            right_index[i] = 0;
+        } else {
+            right_index[i] = stk.top()+1;
         }
         stk.push(i);
     }
+
     return right_index;
 }
 
-int LRProduct(vector<int> a){
-    vector<int> left = nextGreatestLeft(a);
-    vector<int> right = nextGreatestLeft(a);
-    int maxval=-1;
-    for(int i=0; i<a.size(); i++){
+long long LRProduct(vector<long long> a){
+    long long n = a.size();
+    
+    vector<long long> left = nextGreatestLeft(a);
+    vector<long long> right = nextGreatestRight(a);
+
+    long long maxval=0;
+    for(long long i=0; i<n; i++){
         maxval = max(maxval, left[i]*right[i]);
     }
-
     return maxval;
 }
 
@@ -46,15 +67,14 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, temp;
+    long long n, temp;
     cin>>n;
-    vector<int> a;
+    vector<long long> a;
 
-    for(int i=0; i<n; i++){
+    for(long long i=0; i<n; i++){
         cin>>temp;
         a.push_back(temp);
     }
-
     cout<<LRProduct(a);
 
     return 0;
