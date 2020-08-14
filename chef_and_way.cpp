b0c5df199c0b2e6e73740fce6dba32c0822fc713
subhra paladhi <subhrapaladhi9@gmail.com>
2020-08-14 20:43:43 +0530

    network test

--- chef_and_way.cpp
+++ chef_and_way.cpp
@@ -2,55 +2,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-long long compute(
-    vector<long> a, 
-    long n, 
-    long long prod, 
-    unordered_map<string,long long> dp, 
-    long x, 
-    long y
-){
- 
-    if(y==n){
-        return prod*y;
-    }
-    string query = to_string(prod)+"*"+to_string(y);
-
-    if(dp.find(query) != dp.end()){
-        long long res = dp.at(query);
-        res = y*res;
-        
-        compute(a,n,prod,dp,y,)
-    }
-}
-
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    long n, k, temp; 
+    long n, k, temp;
     cin>>n>>k;
     vector<long> a;
     for(int i=0; i<n; i++){
         cin>>temp;
         a.push_back(temp);
     }
-    unordered_map<string,long long> dp;
-    long x=1,y=2;
-    long long prod=1;
-    if(n==1){
-        cout<<1;
-    } else {
-        for(long i=0; i<n; i++){
-            x=i;
-            for(int j=i+1; j<i+k && j<n; i++){
-                y=j;
-                if(y-x<=k && y-x>=1){
-                    prod = min(prod,compute(a,n,1,dp,x,y));
-                }
-            }
+    
+    vector<long>::iterator start = a.end()-k-1,last = a.end()-1, temp1;
+    
+    float sum = log10(a[n-1]);
+    
+    while(true) {
+        temp1 = min_element(start,last);
+        sum += log10(*temp1);
+
+        if(start == a.begin()){
+            break;
+        }
+        
+        last = temp1-1;
+        if(temp1-1-k>a.begin()){
+            start = temp1-1-k;
+        } else {
+            start = a.begin();
         }
     }
+
+    long long temp2 = llround(pow(10,sum))%1000000007;
+    cout<<temp2;
+
     return 0;
 }
